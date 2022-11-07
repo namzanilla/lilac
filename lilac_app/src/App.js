7fed4e405db0f3fb6552f63f7c625a55a1a14a33
manzanilla <namzanilla@gmail.com>
2022-11-07 12:07:45 +0200

    up

--- lilac_app/src/App.js
+++ lilac_app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
+
+import Loader from './components/Loader'
 
 const HomePage = lazy(() => import('./components/HomePage'))
 const AboutPage = lazy(() => import('./components/AboutPage'))
@@ -8,10 +9,10 @@ const AboutPage = lazy(() => import('./components/AboutPage'))
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
