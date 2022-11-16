b1aff194cf6fcbcaa4ab558f053a54866378c710
manzanilla <namzanilla@gmail.com>
2022-11-16 12:40:23 +0200

    api: up

--- lilac_api/src/controllers/languages.controller.js
+++ lilac_api/src/controllers/languages.controller.js
@@ -2,9 +2,11 @@ const { LanguagesModel } = require('./../mongoose/models')
 
 module.exports = {
   getList: async (ctx) => {
-    ctx.body = await LanguagesModel.find().select({
-      name: 1,
-      code: 1,
-    })
+    ctx.body = await LanguagesModel.find()
+      .select({
+        name: 1,
+        code: 1,
+      })
+      .lean()
   },
 }
