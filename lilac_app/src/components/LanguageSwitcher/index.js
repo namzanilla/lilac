82ba70806cc61093ba4dc2b284c74419118bd85b
manzanilla <namzanilla@gmail.com>
2023-01-10 13:06:48 +0200

    app: up

--- lilac_app/src/components/LanguageSwitcher/index.js
+++ lilac_app/src/components/LanguageSwitcher/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import { GrLanguage } from 'react-icons/gr'
+import { Language } from '@styled-icons/ionicons-outline'
 
 export default function LanguageSwitcher() {
   return (
     <Wrap>
-      <GrLanguage />
+      <Language size={20} />
       {/* eslint-disable-next-line */}
-      <a href="#">RU</a>
+      <a href="#">EN</a>
       {/* eslint-disable-next-line */}
       <a href="#">UK</a>
     </Wrap>
@@ -16,8 +16,17 @@ export default function LanguageSwitcher() {
 
 const Wrap = styled.div`
   display: flex;
+  padding: 0 16px;
+  border-left: 1px solid #204468;
+  height: inherit;
+  background-color: #fff;
+  align-items: center;
   a {
     margin-left: 8px;
     font-size: 12px;
+    font-family: "Roboto-Bold";
+  }
+  svg {
+    display: block;
   }
 `
