cffadeac4a52a97b94c6e5ee0be2e84602f64348
manzanilla <namzanilla@gmail.com>
2023-02-13 10:49:22 +0200

    up: s0 up: s0 added clear-public method https://trello.com/c/TY2XyJCl/9-make-clear-public-method

---
+++ lilac_s0/src/clear-public/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const fs = require('fs')
+
+const { resolve } = path
+
+try {
+  clear()
+} catch(e) {
+  console.error(e)
+}
+
+function clear() {
+  const dir = resolve(__dirname, '../../public')
+  const scan = fs.readdirSync(dir)
+  for (const el of scan) {
+    const target = resolve(dir, el)
+    if (fs.lstatSync(target).isDirectory()) {
+      fs.rmSync(target, { recursive: true, force: true })
+    }
+  }
+}
