eda29cb1790ac715b63057e93c6c5f7cc27ba67b
manzanilla <namzanilla@gmail.com>
2023-02-14 10:24:22 +0200

    init

---
+++ lilac_s0/src/import/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+const decompress = require('decompress')
+const { execSync } = require('child_process')
+
+const { resolve } = path
+
+try {
+  importData()
+} catch (e) {
+  console.error(e)
+}
+
+function importData() {
+  const input = resolve(__dirname, '../../tmp/dump.zip')
+
+  if (!fs.existsSync(input)) {
+    throw `File ${input} not found`
+  }
+
+  cleanPublic()
+
+  const dist = resolve(__dirname, '../../public')
+
+  decompress(input, dist).then((files) => {
+    console.log(files)
+  })
+}
+
+function cleanPublic() {
+  const script = resolve(__dirname, '../clear-public')
+  const cmd = `node ${script}`
+  execSync(cmd)
+}
